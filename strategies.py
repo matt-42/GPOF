c839505ac8fa816291ef3c9b8069cfcd6d684656
Matthieu Garrigues <matthieu.garrigues@gmail.com>
2016-04-21 13:07:28 +0200

    Add a new gradient descent.

--- strategies.py
+++ strategies.py
@@ -200,6 +200,183 @@ def gradient_descent(runner, config):
 
 
 '''
+Gradient descent strategy 2.
+Minimise the cost function and try to stay in the givent constraints.
+
+Difference with the previous version: Find the local minimum on a dimension
+before passing to the next one.
+
+config:
+  cost: lambda r: r['p1'] + 10 * r['p2']
+  starting_point: paramset
+  parameters:
+    p1: 23.3
+    p2: a starting point and a range  (parameter to optimize)
+    p3: 1
+
+constraint: lambda r: r['r1'] < 21
+
+max_iteration: 200
+iterator: step | linear_prediction
+'''
+def gradient_descent2(runner, config):
+
+    # build the initial paramset
+    paramset = dict()
+    direction = dict()
+    if config.starting_point is None:
+        for k in list(config.parameters.keys()):
+            direction[k] = 1
+            if type(config.parameters[k]) is not RV:
+                paramset[k] = config.parameters[k];
+            else:
+                paramset[k] = config.parameters[k].value;
+    else:
+        for k in list(config.parameters.keys()):
+            direction[k] = 1
+            if type(config.parameters[k]) is not RV:
+                paramset[k] = type(config.parameters[k])(config.starting_point[k]);
+            else:
+                paramset[k] = type(config.parameters[k].value)(config.starting_point[k]);
+            
+    
+    # Test if it satisfies the constraint.    
+    res = runner.run(paramset)
+    current_cost = config.cost(res)
+    if not config.constraint(res):
+        print("Gradient descent error: The initial paramset does not satisfy the givent constraint.")
+        return
+
+    # iterate loop until a local minimum is found or max_iteration reached.
+    local_minimum=False
+    for iter in range(0, config.max_iterations):
+        if local_minimum:
+            break
+        local_minimum=True
+        for k in sorted(config.parameters.keys()):
+            # For each non fixed parameter
+            if type(config.parameters[k]) is  RV:
+                print("=================== Optimize parameter %s" % k)
+
+                current_value=paramset[k]
+                value1 = prev_value_in_range(config.parameters[k].range, current_value)
+                value2 = next_value_in_range(config.parameters[k].range, current_value)
+
+                if direction[k] and direction[k] > 0:
+                    value1,value2 = value2,value1
+
+                vs=[value1, value2];
+                vcosts=[];
+                for v in vs:
+                    if v is not None:
+                        #   Fill the paramset with the next value
+                        nps = copy.deepcopy(paramset)
+                        nps[k] = v
+                        #   Run the function
+                        res = runner.run(nps)
+                        nps_cost = config.cost(res)
+                        print("Cost: %f, value: %f" % (nps_cost, v))
+                        vcosts.append([v, nps_cost])
+                        if nps_cost < current_cost:
+                            break
+
+                # Find the best direction
+                bestc = None
+                bestv = None
+                for p in vcosts:
+                    if bestc is None or bestc > p[1]:
+                        bestv = p[0]
+                        bestc = p[1]
+
+
+                if bestv is not None and bestc < current_cost:
+                    direction[k] = bestv - current_value # save the direction for the next step.
+                    local_minimum=False
+                    prev_value = current_value
+                    prev_cost = current_cost
+                    current_value = bestv
+                    current_cost = bestc
+                    
+                    paramset[k] = bestv
+                    print("BEST COST FOUND: %f" % bestc)
+
+                    # Iterate on k while we can find a lower cost.
+                    k_local_min=False
+                    while not k_local_min:
+                        k_local_min=True
+                        # try linear prediction
+
+                        # linear prediction iterator
+                        # predict where the zero  cost is
+                        #prediction = current_value + ( current_cost - bestc) * (bestv - current_value)
+                        prediction = prev_value + prev_cost * (current_value - prev_value) / ( prev_cost - current_cost)
+
+                        # if the prediction is in the interval, use it
+                        if in_interval(config.parameters[k].range, prediction):
+                            nps = copy.deepcopy(paramset)
+                            v = range_round(config.parameters[k].range, prediction).item()
+                            nps[k] = v
+                            res = runner.run(nps)
+                            pred_cost = config.cost(res)
+                            print("Cost: %f, value: %f" % (pred_cost, nps[k]))
+                            if pred_cost < bestc: # if it does not lower the cost, fallback to a step
+                                paramset[k] = v
+                                current_cost = pred_cost
+                                current_value = v
+                                print("BEST COST FOUND: %f" % pred_cost)
+                                k_local_min=False
+
+                        if k_local_min:
+                            nps = copy.deepcopy(paramset)
+
+                            if direction[k] < 0:
+                                nps[k] = prev_value_in_range(config.parameters[k].range, bestv)
+                            else:
+                                nps[k] = next_value_in_range(config.parameters[k].range, bestv)
+
+                            res = runner.run(nps)
+                            step_cost = config.cost(res)
+                            print("Cost: %f, value: %f" % (pred_cost, nps[k]))
+                            if step_cost < bestc:
+                                paramset[k] = nps[k]
+                                current_cost = step_cost
+                                current_value = nps[k]
+                                print("BEST COST FOUND: %f" % step_cost)
+                                k_local_min=False
+
+                        
+                #   If their is a new minimum
+                if bestv is not None and bestc < current_cost:
+                    direction[k] = bestv - current_value # save the direction for the next step.
+                    local_minimum=False
+                    paramset[k] = bestv
+                    current_cost = bestc
+
+                    print("BEST COST FOUND: %f" % bestc)
+                    # Save it
+                    if config.iterator == "linear_prediction":
+                        # linear prediction iterator
+                        # predict where the zero  cost is
+                        prediction = current_value + ( current_cost - bestc) * (bestv - current_value)
+
+                        # if the prediction is in the interval, use it
+                        if in_interval(config.parameters[k].range, prediction):
+                            nps = copy.deepcopy(paramset)
+                            v = range_round(config.parameters[k].range, prediction).item()
+                            nps[k] = v
+                            res = runner.run(nps)
+                            pred_cost = config.cost(res)
+                            print("Cost: %f, value: %f" % (pred_cost, nps[k]))
+                            if pred_cost < bestc: # if it does not lower the cost, fallback to a step
+                                paramset[k] = v
+                                current_cost = pred_cost
+                                print("BEST COST FOUND: %f" % pred_cost)
+                                
+                        else: # if not, step
+                            paramset[k] = bestv
+
+                            
+'''
 Parallel gradient descent strategy.
 Minimise the cost function and try to stay in the givent constraints.
 
